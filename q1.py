6f755b7541fa06f05788cf2a071eb4507c4f21e6
Shri Lekkala <shri.lekkala@gmail.com>
2020-12-30 13:42:27 +0530

    Added LU function for tri-diag matrix

--- q1.py
+++ q1.py
@@ -6,3 +6,18 @@ def triA(c, d, m):
     D = np.diag(np.ones(m-1) * d, 1)
     A += D + D.T
     return A
+
+def LUtri(c, d, m):
+    A = triA(c, d, m)
+
+    U = A.copy()
+    L = np.eye(m)
+
+    for k in range(m-1):
+        L[k+1, k] = U[k+1, k] / U[k, k]
+        U[k+1, k:k+2] -= L[k+1, k] * U[k, k:k+2]
+
+    return A, L, U
+
+A, L, U = LUtri(3,4,5)
+
