00fac436d4deaf3a37cb49156f8aba74474b0865
Shri Lekkala <shri.lekkala@gmail.com>
2020-12-30 13:43:56 +0530

    Added separate forward and back substitution functions

--- q1.py
+++ q1.py
@@ -19,5 +19,31 @@ def LUtri(c, d, m):
 
     return A, L, U
 
-A, L, U = LUtri(3,4,5)
+def forward_sub(L, b):
+    m, _ = L.shape
+    x = np.zeros(m)
 
+    x[0] = b[0] / L[0, 0]
+    for k in range(1, m):
+        j = k-1
+        x[k] = (b[k] - L[k, j] * x[j]) / L[k, k]
+
+    return x
+
+def back_sub(U, y):
+    m, _ = U.shape
+    x = np.zeros(m)
+
+    x[m-1] = y[m-1] / U[m-1, m-1]
+    for k in range(m-2, -1, -1):
+        j = k+1
+        x[k] = (y[k] - U[k, j] * x[j]) / U[k, k]
+
+    return x   
+
+A, L, U = LUtri(3,4,5)
+b = np.random.rand(5)
+x = forward_sub(L, b)
+x2 = back_sub(U, b)
+U @ x2
+b
