bdb3bfdfd1ae2783ca23bbb74f54a6e2f0074c93
Shri Lekkala <shri.lekkala@gmail.com>
2021-01-03 02:20:21 +0530

    Updated whole file with comments and docstrings

--- q1.py
+++ q1.py
@@ -1,36 +1,55 @@
 import numpy as np
 
-"Q1"
 def triA(c, d, m):
+    """
+    Creates matrix tri-diagonal matrix A as shown in question 1
+    parameters: values c and d and size of matrix (m)
+
+    :return A: the required matrix A
+    """
     A = np.eye(m) * c
     D = np.diag(np.ones(m-1) * d, 1)
     A += D + D.T
     return A
 
+"Part a)"
 def LUtri(c, d, m):
-    A = triA(c, d, m)
-
-    U = A.copy()
+    """
+    Algorithm 4 from the coursework
+    Constructs matrix A and implements LU decomposition on this matrix
+    Returns L and U
+    """
+    U = triA(c, d, m)
     L = np.eye(m)
 
     for k in range(m-1):
         L[k+1, k] = U[k+1, k] / U[k, k]
         U[k+1, k:k+2] -= L[k+1, k] * U[k, k:k+2]
 
-    return A, L, U
+    return L, U
 
 def forward_sub(L, b):
+    """
+    Algorithm 5 from the coursework
+    Solves Ly=b via forward substitution given L and b
+    Returns y
+    """
     m, _ = L.shape
-    x = np.zeros(m)
+    y = np.zeros(m)
 
-    x[0] = b[0] / L[0, 0]
+    y[0] = b[0] / L[0, 0]
     for k in range(1, m):
         j = k-1
-        x[k] = (b[k] - L[k, j] * x[j]) / L[k, k]
+        y[k] = (b[k] - L[k, j] * y[j]) / L[k, k]
 
-    return x
+    return y
 
 def back_sub(U, y):
+    """
+    Algorithm 6 from the coursework
+    Solves Ux=y via forward substitution given U and y
+    Returns x
+    """
     m, _ = U.shape
     x = np.zeros(m)
 
@@ -41,26 +60,23 @@ def back_sub(U, y):
 
     return x   
 
-c = 5
-d = 6
-m = 10
-A, L, U = LUtri(c,d,m)
-b = np.random.rand(m)
-x1 = forward_sub(L, b)
-
-x = back_sub(U, x1)
-A@x
-b
-
+"Part d)"
 def LU_solve(c, d, m, b):
-    A = triA(c, d, m)
-
-    U = A.copy()
+    """
+    Algorithm 7 from the coursework
+    Given a vector b and a parametrisation of matrix A using c, d and m
+    Solves Ax = b for vector x
+    Returns x
+    """
+    # Initialise matrices L,U and vectors x, y
+    U = triA(c, d, m)
     L = np.eye(m)
 
     y = np.zeros(m)
-    y[0] = b[0] / L[0, 0]
+    x = np.zeros(m)
 
+    # Merged LU factorisation and forward substitution
+    y[0] = b[0] / L[0, 0]
     for k in range(m-1):
         L[k+1, k] = U[k+1, k] / U[k, k]
         U[k+1, k:k+2] -= L[k+1, k] * U[k, k:k+2]
@@ -68,31 +84,10 @@ def LU_solve(c, d, m, b):
         j = k
         y[k+1] = (b[k+1] - L[k+1, j] * y[j]) / L[k+1, k+1]
 
-    x = np.zeros(m)
+    # Back Substitution
     x[m-1] = y[m-1] / U[m-1, m-1]
-
     for k in range(m-2, -1, -1):
         j = k+1
         x[k] = (y[k] - U[k, j] * x[j]) / U[k, k]
 
-    return A, L, U, x
-
-c = 2
-d = 6
-m = 7
-b = np.random.rand(m)
-A, L, U, x = LU_solve(c, d, m, b)
-A@x
-b
-
-
-def LU_inplace(c, d, m):
-    A1 = triA(c, d, m)
-
-    A = A1.copy()
-
-    for k in range(m-1):
-        A[k+1, k] = A[k+1, k] / A[k, k]
-        A[k+1, k+1] = A[k+1, k+1] - np.outer(A[k+1, k], A[k, k+1])
-    
-    return(A)
+    return x
