330f8bef502433cf07282c6bf7ecd28f2051c373
Shri Lekkala <shri.lekkala@gmail.com>
2021-01-03 02:21:03 +0530

    Added test for part d) and updated previous test

--- test_q1.py
+++ test_q1.py
@@ -5,12 +5,18 @@ import q1
 import numpy as np
 
 ''' 
-Test the algorithms from Q1 part a)
+Test the 3 algorithms from Q1 part a)
 '''
-@pytest.mark.parametrize('c, d, m', [(5, 8, 6), (52, 102, 61), (1, 812, 150)])
-def test_LUsolve_a(c, d, m):
-    np.random.seed(8564*m)
+@pytest.mark.parametrize('m', [5, 11, 24, 102, 606])
+def test_LUsolve_a(m):
+    np.random.seed(5601*m)
+    c = np.random.randint(m)
+    d = np.random.randint(m)
+
+    # create matrix A using the paramteters
     A = q1.triA(c, d, m)
+
+    # obtain matrices L and U
     L, U = q1.LUtri(c,d,m)
 
     # Check that L is lower triangular to a specific threshold
@@ -22,6 +28,28 @@ def test_LUsolve_a(c, d, m):
     b = np.random.rand(m)
     y = q1.forward_sub(L, b)
     x = q1.back_sub(U, y)
+
+    err = A@x - b
+    # Check that the the correct x is obtained
+    assert(np.linalg.norm(err) < 1.0e-6)
+
+''' 
+Test the LU solve algorithm from Q1 part d)
+'''
+@pytest.mark.parametrize('m', [12, 25, 231, 500, 845])
+def test_LUsolve_b(m):
+    np.random.seed(8564*m)
+    c = np.random.randint(m)
+    d = np.random.randint(m)
+
+    # create matrix A using the paramteters
+    A = q1.triA(c, d, m)
+
+    # create a random vector b
+    b = np.random.rand(m)
+    
+    # obtain x via the function LU_solve
+    x = q1.LU_solve(c, d, m, b)
 
     err = A@x - b
     # Check that the the correct x is obtained
