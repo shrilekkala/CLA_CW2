34cf3d88e9776777ace34dc485664d818155ab0c
Shri Lekkala <shri.lekkala@gmail.com>
2021-01-05 22:54:56 +0530

    Added functions for 2 g) and checked with a specific example

--- q2.py
+++ q2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 import q1
 from cla_utils import exercises7
 
@@ -64,6 +65,9 @@ def solver_alg(c1, b):
     return x
 
 def compare_algs(m):
+    """
+    Function that compares the times taken for the solver_alg and solve_LUP functions for a given m
+    """
     c1 = 20
 
     # construct matrix A
@@ -85,3 +89,91 @@ def compare_algs(m):
     print("Time taken for solve_LUP  : " + str(time.time() - start_t))
 
     return
+
+
+
+def get_f(u, w, delx):
+    """
+    Gets the required vector f given u, w and delta x
+    """
+    u_diff_approx = (np.roll(u, -1) - 2 * u + np.roll(u, 1))/(delx**2)
+    w_diff_approx = (np.roll(w, -1) - 2 * w + np.roll(w, 1))/(delx**2)
+
+    f = w + delt * u_diff_approx + ((delt/2)**2) * w_diff_approx
+    return f
+
+def compute_timesteps(M, delt, n, u_func, w_func):
+    """
+    Function that computes and plots a number of timesteps of the wave equation
+
+    :param M: constant to determine grid width
+    :param delt: delta t, the time difference between time steps
+    :param u: initial condition u(x,0)
+    :param w: initial condition w(x,0)
+    :param n: number of time steps to be computed
+    :param u_func: the actual function u(x,t) for comparison
+    :param w_func: the actual function w(x,t) for comparison
+
+    :return x: an m dimensional vector solution
+    """ 
+    # obtain grid width
+    delx = 1/M
+
+    # obtain constant C1
+    c = (delt / 2)**2
+    c1 = c / (delx)**2
+
+    # generate evenly distributed x values at grid width
+    x_vals = (np.arange(M)+1) * delx
+    # generate evenly distributed x values for the actual solution
+    x_range = np.linspace(x_vals[0],1,501)
+
+    # obtain the intial conditions
+    u = u_func(x_vals, 0)
+    w = w_func(x_vals, 0)
+
+    for k in range(n):
+        
+        # obtain the w and u at the next time step
+        f = get_f(u, w, delx)
+        new_w = solver_alg(c1, f)
+        new_u = u + (delt / 2) * (w + new_w)
+
+        # obtain the actual w and u from the solution at the next time step
+        actual_w_vals = w_func(x_range, k*delt)
+        actual_u_vals = u_func(x_range, k*delt) 
+
+        # plot the discretisation and the actual function every plot_step timesteps
+        plot_step = 25
+        if k % plot_step == 0:
+            plt.plot(x_vals, new_w)
+            plt.plot(x_range, actual_w_vals, ls='--')
+
+        # update u and w
+        u = new_u
+        w = new_w
+
+        #print(np.linalg.norm(new_w - actual_w_vals))
+    
+    plt.title("Plot of w(x,t) against x for different time steps")
+    plt.xlabel("x")
+    plt.ylabel("w(x,t)")
+
+def w_func(x,t):
+    w_vals = (np.sin(2 * np.pi * x) / (2 * np.pi)) * np.cos(2 * np.pi * t)
+    return w_vals
+
+def u_func(x,t):
+    u_vals = (np.sin(2 * np.pi * x) / (2 * np.pi)) * (np.sin(2 * np.pi * t) / (2 * np.pi))
+    return u_vals
+
+M = 500
+delt = 1/1000
+n = 251
+#u = np.zeros(M)
+#w = np.sin(x_vals * 2 * np.pi) / (2 * np.pi)
+
+compute_timesteps(M, delt, n, u_func, w_func)
+
+plt.show()
+
