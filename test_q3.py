a92ec26e21e6538b7f8ed94b9a9b549dc3b3de95
Shri Lekkala <shri.lekkala@gmail.com>
2021-01-07 22:18:01 +0530

    Added tests for Q3 d)

--- test_q3.py
+++ test_q3.py
@@ -7,7 +7,7 @@ import q3
 Test the qr_factor_tri function from Q2 part c)
 '''
 @pytest.mark.parametrize('m', [14, 20, 135, 500, 935])
-def test_solver_alg(m):
+def test_qr_factor_tri(m):
     np.random.seed(5501*m)
 
     # Create a random, symmetric tridiagonal matrix A
@@ -16,7 +16,7 @@ def test_solver_alg(m):
     A = np.diag(d1) + np.diag(d2,1) + np.diag(d2,-1)
 
     # Obtain R and V from qr_alg_tri
-    R, V = q3.qr_alg_tri(A.copy())
+    R, V = q3.qr_factor_tri(A.copy())
 
     # Construct Q from V
     Q = np.eye(m)
@@ -32,3 +32,30 @@ def test_solver_alg(m):
 
     # Check that the the correct Q and R are obtained
     assert(np.linalg.norm(err) < 1.0e-6)
+
+''' 
+Test the qr_alg_tri function from Q2 part d)
+'''
+@pytest.mark.parametrize('m', [14, 20, 135, 500, 935])
+def test_qr_alg_tri(m):
+    np.random.seed(6601*m)
+
+    # Create a random, symmetric tridiagonal matrix A
+    d1 = np.random.rand(m)
+    d2 = np.random.rand(m-1)
+    A = np.diag(d1) + np.diag(d2,1) + np.diag(d2,-1)
+
+    # Obtain Ak from qr_alg_tri
+    Ak = q3.qr_alg_tri(A.copy(), 1000)
+
+    # Check it is still Hermitian
+    assert(np.linalg.norm(Ak - np.conj(Ak).T) < 1.0e-4)
+
+    # Check that the m, m-1 element is of appropriate size
+    assert(Ak[m-1,m-2] < 1.0e-12)
+
+    # Check its still tridiagonal
+    assert(np.linalg.norm(Ak[np.tril_indices(m, -2)])/m**2 < 1.0e-5)
+
+    # Check for conservation of trace
+    assert(np.abs(np.trace(A) - np.trace(Ak)) < 1.0e-6)
